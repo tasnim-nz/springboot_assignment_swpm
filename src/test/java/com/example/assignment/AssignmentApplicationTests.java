4174ed2f8ca800c4d134e7e804bf822be01105e9
tasnim_nz <nuzhattasnim800@gmail.com>
2026-02-24 23:05:45 +0600

    Fixed unit tests

--- src/test/java/com/example/assignment/AssignmentApplicationTests.java
+++ src/test/java/com/example/assignment/AssignmentApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.assignment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class AssignmentApplicationTests {
 
 	@Test
